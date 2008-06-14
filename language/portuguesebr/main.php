780ababbef1d1afafcaf6f4e0969536eeba4a177
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-14 07:23:46 +0000

    modules - Contact - portuguesebr - part 2 - main.php finished...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/contact/trunk@2846 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/main.php
+++ language/portuguesebr/main.php
@@ -1,29 +1,36 @@
 <?php
-// $Id: main.php 1662 2008-04-18 21:10:59Z malanciault $
+/**
+* $Id: main.php 159 2007-12-17 16:44:05Z GibaPhp $
+* Module: Contact
+* Author: The Impresscms Project
+* Translation: Portuguesebr - GibaPhp
+* Licence: GNU
+*/
+
 //%%%%%%		File Name contact.php 		%%%%%
-define("_CT_CONTACTFORM","Contact Us Form");
-define("_CT_THANKYOU","Thank you for your interest in our site!");
-define("_CT_NAME","Name");
+define("_CT_CONTACTFORM","Formulário de Contato");
+define("_CT_THANKYOU","Obrigado pelo seu interesse em nosso site!");
+define("_CT_NAME","Nome");
 define("_CT_EMAIL","Email");
 define("_CT_URL","Web site");
 define("_CT_ICQ","ICQ");
-define("_CT_COMPANY","Company");
-define("_CT_LOCATION","City");
-define("_CT_COMMENTS","Question(s)");
-define("_CT_SUBMIT","Submit");
-define("_CT_YOURMESSAGE","Your Message:");
+define("_CT_COMPANY","Empresa");
+define("_CT_LOCATION","Cidade");
+define("_CT_COMMENTS","Pergunta(s)");
+define("_CT_SUBMIT","Ok!");
+define("_CT_YOURMESSAGE","Sua Mensagem:");
 define("_CT_WEBMASTER","Webmaster");
-define("_CT_HELLO","Hello %s,");
-define("_CT_THANKYOUCOMMENTS","Thank you for your comments about %s.");
-define("_CT_SENTTOWEBMASTER","Your message has been sent to the webmaster(s) of %s.");
-define("_CT_SUBMITTED","%s submitted the following Information:");
-define("_CT_MESSAGESENT","Message to %s Sent");
-define("_CT_SENTASCONFIRM","Your comments have been sent to: %s as a confirmation email.");
+define("_CT_HELLO","Olá %s,");
+define("_CT_THANKYOUCOMMENTS","Obrigado por seus comentários sobre %s.");
+define("_CT_SENTTOWEBMASTER","Sua mensagem foi enviada para os administradores do %s.");
+define("_CT_SUBMITTED","%s apresentaram as seguintes informações:");
+define("_CT_MESSAGESENT","Mensagem para %s Enviada");
+define("_CT_SENTASCONFIRM","Seus comentários foram enviadas para a: %s como uma confirmação por e-mail.");
 
 // Hack by marcan : More information on the Contact module
-define("_CT_CONTACTUS","Contact us");
-define("_CT_CONTACTUS_INFO","Contact us by filling out this form. We will answer you as soon as possible!");
-define("_CT_REQUIRED","Required");
+define("_CT_CONTACTUS","Fale Conosco");
+define("_CT_CONTACTUS_INFO","Fale conosco preenchendo este formulário. Iremos responder o mais breve possível!");
+define("_CT_REQUIRED","Obrigatório");
 define("_CT_CONTACT_PHONE","T");
 define("_CT_CONTACT_FAX","F");
 
