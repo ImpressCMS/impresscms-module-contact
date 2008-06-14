64516569089178ee6551262c863d6b907b0696d2
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-14 07:07:33 +0000

    modules - Contact - portuguesebr - Start - all files for translation...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/contact/trunk@2844 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/portuguesebr/blocks.php
@@ -0,0 +1,7 @@
+<?php
+
+define("_CT_MB_CONTACT_PHONE","T");
+define("_CT_MB_CONTACT_FAX","F");
+define("_CT_MB_CONTACT_FORM","Contact Form");
+
+?>
---
+++ language/portuguesebr/main.php
@@ -0,0 +1,31 @@
+<?php
+// $Id: main.php 1662 2008-04-18 21:10:59Z malanciault $
+//%%%%%%		File Name contact.php 		%%%%%
+define("_CT_CONTACTFORM","Contact Us Form");
+define("_CT_THANKYOU","Thank you for your interest in our site!");
+define("_CT_NAME","Name");
+define("_CT_EMAIL","Email");
+define("_CT_URL","Web site");
+define("_CT_ICQ","ICQ");
+define("_CT_COMPANY","Company");
+define("_CT_LOCATION","City");
+define("_CT_COMMENTS","Question(s)");
+define("_CT_SUBMIT","Submit");
+define("_CT_YOURMESSAGE","Your Message:");
+define("_CT_WEBMASTER","Webmaster");
+define("_CT_HELLO","Hello %s,");
+define("_CT_THANKYOUCOMMENTS","Thank you for your comments about %s.");
+define("_CT_SENTTOWEBMASTER","Your message has been sent to the webmaster(s) of %s.");
+define("_CT_SUBMITTED","%s submitted the following Information:");
+define("_CT_MESSAGESENT","Message to %s Sent");
+define("_CT_SENTASCONFIRM","Your comments have been sent to: %s as a confirmation email.");
+
+// Hack by marcan : More information on the Contact module
+define("_CT_CONTACTUS","Contact us");
+define("_CT_CONTACTUS_INFO","Contact us by filling out this form. We will answer you as soon as possible!");
+define("_CT_REQUIRED","Required");
+define("_CT_CONTACT_PHONE","T");
+define("_CT_CONTACT_FAX","F");
+
+
+?>
