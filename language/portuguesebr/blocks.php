94ae6e795d1be6987b880fd68d0abab28548346c
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-14 07:23:03 +0000

    modules - Contact - portuguesebr - part 1 - blocks.php finished...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/contact/trunk@2845 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/blocks.php
+++ language/portuguesebr/blocks.php
@@ -1,7 +1,14 @@
 <?php
+/**
+* $Id: blocks.php 159 2007-12-17 16:44:05Z GibaPhp $
+* Module: Contact
+* Author: The Impresscms Project
+* Translation: Portuguesebr - GibaPhp
+* Licence: GNU
+*/
 
 define("_CT_MB_CONTACT_PHONE","T");
 define("_CT_MB_CONTACT_FAX","F");
-define("_CT_MB_CONTACT_FORM","Contact Form");
+define("_CT_MB_CONTACT_FORM","Formulário de Contato ");
 
 ?>
